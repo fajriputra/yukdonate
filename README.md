66adbb92d1b19b1003be3e96485fa049454ebb2d
AL FAJRI PUTRA PRATAMA <72371119+fajriputra@users.noreply.github.com>
2022-06-22 17:42:38 +0700

    preparing configuration project needed 🚀

---
+++ README.md
@@ -0,0 +1,2 @@
+# yukdonate
+ 
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable arrow-body-style */
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <>test</>;
+};
+
+export default Home;
